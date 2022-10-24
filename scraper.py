6d626e326d0f5f61e7834b0e7be4b8d40843c09d
Tejas M R <tejasmr@outlook.com>
2022-10-24 14:38:39 +0530

    Added page_source to check why it is not working

--- scraper.py
+++ scraper.py
@@ -4,6 +4,7 @@ from definitions import TAG_NAME
 
 def collect_video_metadata(link):
     driver.get(link)
+    logger.debug(driver.page_source)
     videos = driver.find_elements(by=TAG_NAME, value=f"ytd-video-renderer")
     # for i in range(2, 2 + 10 - 1):
     #     try:
