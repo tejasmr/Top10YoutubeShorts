6ff10e64c3ee212434b36f100eece502bdd3850d
Tejas M R <tejasmr@outlook.com>
2022-10-24 19:23:08 +0530

    Added metadata code for the videos

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
-from definitions import FINANCE_LINK
+from definitions import FINANCE_FILE, FINANCE_LINK
 from logger import logger
 from scraper import collect_video_metadata
 
 if __name__ == "__main__":
-    collect_video_metadata(FINANCE_LINK)
+    collect_video_metadata(FINANCE_LINK, FINANCE_FILE, "Finance")
     logger.info('Ran the program successfully')
--- scraper.py
+++ scraper.py
@@ -1,44 +1,67 @@
+from datetime import datetime
 from glob import glob
 import os
 from time import sleep
 from logger import logger
 from driver import driver
-from definitions import FINANCE_FILE, TAG_NAME
+from definitions import CLASS_NAME, file, TAG_NAME
 from pytube import YouTube
-from moviepy.editor import VideoFileClip, concatenate_videoclips
+from moviepy.editor import VideoFileClip, concatenate_videoclips, TextClip, CompositeVideoClip
+
+from symbol import compound_stmt
 
 def get_link(link):
     driver.get(link)
     sleep(5)
 
-def collect_video_metadata(link):
+def scrape_video(link, file, label):
+    title = ""
     get_link(link)
     videos = driver.find_elements(by=TAG_NAME, value=f"ytd-video-renderer")
-    # for i in range(2, 2 + 10 - 1):
-    #     try:
-    #         videos.append(driver.find_element(by=CLASS_NAME, value=f"style-scope ytd-item-section-renderer"))
-    #     except:
-    #         pass
-    # .style-scope.ytd-item-section-renderer
-    videos = videos[:10]
+    
     logger.debug("number of videos found = " + str(len(videos)))
+
+    videos = videos[:10]
     links = []
+    titles = []
+    channels = []
+    video_links = []
+    
     for video in videos:
         video_link = video.find_element(by=TAG_NAME, value='a').get_attribute('href')
+        video_title = video.find_element(by=TAG_NAME, value='a').get_attribute('title')
+        video_channel = video.find_element(by=CLASS_NAME, value="yt-simple-endpoint style-scope yt-formatted-string")
         yt = YouTube(video_link)
         links.append(yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download())
+        video_links.append(video_link)
+        titles.append(video_title)
+        channels.append(video_channel)
     
+    links = links[::-1]
+    titles = titles[::-1]
+    channels = channels[::-1]
+    video_links = video_links[::-1]
 
-    if os.path.exists(FINANCE_FILE):
-        os.remove(FINANCE_FILE)
+    if os.path.exists(file):
+        os.remove(file)
 
-    logger.info(links)
-    clips = [VideoFileClip(file) for file in links]
+    clips = []
+    title = f"Top {len(links)} {label} shorts of the day {datetime.today()}"
+    description = "Disclaimer : No copyright intention, I do not own the audio, video or the images in this video. All rights belong to the rightful owner. \n\nCredits: \n" 
+    for i in range(10):
+        description += f'{10-i}. {channels[i]} - {titles[i]} : {video_links[i]}\n'
+    for i in range(0, len(links)):
+        textclip = TextClip(str(10-i), fontsize=100, color='white', stroke_color='black', align='West')
+        textclip = textclip.set_position((0, 0))
+        videoclip = VideoFileClip(links[i])
+        compositeclip = CompositeVideoClip([textclip, videoclip])
+        clips.append(compositeclip.set_duration(videoclip.duration))
+
     final = concatenate_videoclips(clips, method="compose")
     logger.info("Final video duration: " + str(final.duration))
-    final.write_videofile(FINANCE_FILE)
+    final.write_videofile(file)
 
     for file in glob("*.mp4"):
         logger.info(file)
-        if file != FINANCE_FILE:
+        if file != file:
             os.remove(file)
