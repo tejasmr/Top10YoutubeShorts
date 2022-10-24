c56085df1d0710739c632c1c73eecafda4811b60
Tejas M R <tejasmr@outlook.com>
2022-10-24 14:11:01 +0530

    Added selenium by definitions

--- definitions.py
+++ definitions.py
@@ -1,1 +1,9 @@
+from selenium.webdriver.common.by import By
+
+TAG_NAME = By.TAG_NAME
+CLASS_NAME = By.CLASS_NAME
+XPATH = By.XPATH
+ID = By.ID
+CSS_SELECTOR = By.CSS_SELECTOR
+
 FINANCE_LINK = "https://www.youtube.com/results?search_query=%23shorts+finance&sp=CAMSBAgCEAE%253D"
