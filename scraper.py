58a60a3307d4ff19c183b1f0b932a2d9472b1a3e
Tejas M R <tejasmr@outlook.com>
2022-10-24 15:01:19 +0530

    Download the top 10 videos

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ from time import sleep
 from logger import logger
 from driver import driver
 from definitions import TAG_NAME
+from pytube import YouTube
 
 def get_link(link):
     driver.get(link)
@@ -18,6 +19,7 @@ def collect_video_metadata(link):
     # .style-scope.ytd-item-section-renderer
     logger.debug("number of videos found = " + str(len(videos)))
     logger.debug("videos found = " + str(videos))
-    for video in videos:
-        logger.debug(video.find_element(by=TAG_NAME, value='a'))
-        logger.debug(video.find_element(by=TAG_NAME, value='a').get_attribute('href'))
+    for video in videos[:10]:
+        video_link = video.find_element(by=TAG_NAME, value='a').get_attribute('href')
+        yt = YouTube(video_link)
+        yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
