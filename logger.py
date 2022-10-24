5f01fb80a803767b6421f130e723bdf7f38795ee
Tejas M R <tejasmr@outlook.com>
2022-10-24 13:29:14 +0530

    Fixed latest logger appending logs

--- logger.py
+++ logger.py
@@ -1,4 +1,10 @@
 import logging
+import os
+
+LOG_FILE = r'latest_log.txt'
+
+if os.path.exists(LOG_FILE):
+    os.remove(LOG_FILE)
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -8,7 +14,9 @@ ch.setLevel(logging.DEBUG)
 ch.setFormatter(logging.Formatter('[%(levelname)s] %(message)s'))
 logger.addHandler(ch)
 
-fh = logging.FileHandler(r'latest_log.txt')
+
+
+fh = logging.FileHandler(LOG_FILE)
 fh.setLevel(logging.DEBUG)
 fh.setFormatter(logging.Formatter('[%(levelname)s] %(message)s'))
 logger.addHandler(fh)
