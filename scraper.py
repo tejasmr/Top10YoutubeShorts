12bc7b8d8c7e5206010720ff80e149f5a63c05db
Tejas M R <tejasmr@outlook.com>
2022-10-24 15:14:54 +0530

    Delete videos after use

--- scraper.py
+++ scraper.py
@@ -1,3 +1,5 @@
+from glob import glob
+import os
 from time import sleep
 from logger import logger
 from driver import driver
@@ -17,9 +19,14 @@ def collect_video_metadata(link):
     #     except:
     #         pass
     # .style-scope.ytd-item-section-renderer
+    videos = videos[:10]
     logger.debug("number of videos found = " + str(len(videos)))
-    logger.debug("videos found = " + str(videos))
-    for video in videos[:10]:
+    
+    for video in videos:
         video_link = video.find_element(by=TAG_NAME, value='a').get_attribute('href')
         yt = YouTube(video_link)
         yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
+    
+    for file in glob("*.mp4"):
+        logger.log(file)
+        os.remove(file)
