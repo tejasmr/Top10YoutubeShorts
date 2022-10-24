83242deedba613a3007b5fd3d92ca7aad4429841
Tejas M R <tejasmr@outlook.com>
2022-10-24 16:18:05 +0530

    Fixed file order

--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@ import os
 from time import sleep
 from logger import logger
 from driver import driver
-from definitions import TAG_NAME
+from definitions import FINANCE_FILE, TAG_NAME
 from pytube import YouTube
 from moviepy.editor import VideoFileClip, concatenate_videoclips
 
@@ -22,18 +22,23 @@ def collect_video_metadata(link):
     # .style-scope.ytd-item-section-renderer
     videos = videos[:10]
     logger.debug("number of videos found = " + str(len(videos)))
-    
+    links = []
     for video in videos:
         video_link = video.find_element(by=TAG_NAME, value='a').get_attribute('href')
         yt = YouTube(video_link)
-        yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
+        links.append(yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download())
     
-    clips = [VideoFileClip(file) for file in glob("*.mp4")]
+
+    if os.path.exists(FINANCE_FILE):
+        os.remove(FINANCE_FILE)
+
+    logger.info(links)
+    clips = [VideoFileClip(file) for file in links]
     final = concatenate_videoclips(clips, method="compose")
     logger.info("Final video duration: " + str(final.duration))
-    final.write_videofile("Final.mp4")
+    final.write_videofile(FINANCE_FILE)
 
     for file in glob("*.mp4"):
         logger.info(file)
-        if file != "Final.mp4":
+        if file != FINANCE_FILE:
             os.remove(file)
