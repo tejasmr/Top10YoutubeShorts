64a8f6045e77a5c0882731e5b12c3fa1d526e91e
Tejas M R <tejasmr@outlook.com>
2022-10-24 14:18:18 +0530

    Fixed logs

--- scraper.py
+++ scraper.py
@@ -5,4 +5,4 @@ from definitions import CSS_SELECTOR
 def collect_video_metadata(link):
     driver.get(link)
     videos = driver.find_elements(by=CSS_SELECTOR, value="ytd-video-renderer.style-scope.ytd-item-section-renderer")
-    logger.debug("number of videos found = ", len(videos))
+    logger.debug("number of videos found = " + len(videos))
