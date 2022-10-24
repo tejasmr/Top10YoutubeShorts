d7f96e247f88bafda019e165bc26bd2ccfbd4f65
Tejas M R <tejasmr@outlook.com>
2022-10-24 14:15:01 +0530

    Calling collect_video_metadata from finance link

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
+from definitions import FINANCE_LINK
 from logger import logger
+from scraper import collect_video_metadata
 
 if __name__ == "__main__":
+    collect_video_metadata(FINANCE_LINK)
     logger.info('Ran the program successfully')
