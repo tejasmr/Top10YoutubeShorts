a8ac2583e8b5d37e3b796b4b1b04a1a869540666
Tejas M R <tejasmr@outlook.com>
2022-10-24 14:29:11 +0530

    Using css sector by copying it

--- scraper.py
+++ scraper.py
@@ -4,7 +4,12 @@ from definitions import CSS_SELECTOR
 
 def collect_video_metadata(link):
     driver.get(link)
-    videos = driver.find_elements(by=CSS_SELECTOR, value="ytd-video-renderer")
+    videos = []
+    for i in range(2, 2 + 10 - 1):
+        try:
+            videos.append(driver.find_element(by=CSS_SELECTOR, value=f"#contents > ytd-video-renderer:nth-child({i})"))
+        except:
+            pass
     # .style-scope.ytd-item-section-renderer
     logger.debug("number of videos found = " + str(len(videos)))
     logger.debug("videos found = " + str(videos))
