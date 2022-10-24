54a84006eae5703f0f67c8b9578e88ca89b7959a
Tejas M R <tejasmr@outlook.com>
2022-10-24 14:11:24 +0530

    Checking if get and find_elements is working with given css selector

--- scraper.py
+++ scraper.py
@@ -1,2 +1,8 @@
+from logger import logger
+from driver import driver
+from definitions import CSS_SELECTOR
+
 def collect_video_metadata(link):
-    pass
+    driver.get(link)
+    videos = driver.find_elements(by=CSS_SELECTOR, value="ytd-video-renderer.style-scope.ytd-item-section-renderer")
+    logger.debug("number of videos found = ", len(videos))
